a465119db3de8d7f3db3c8745d86611d4b34797e
aliyashz <101852982+aliyashz@users.noreply.github.com>
2024-01-11 15:50:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,21 @@ def model():
     # Add more content as needed
 
 # Create tabs
-tabs = ["Home", "About", "Analysis", "Model"]
-selected_tab = st.sidebar.radio("Select Tab", tabs)
+
+# Load ZINC dataset
+csv_path = st.cache_data(pd.read_csv)(
+    "https://raw.githubusercontent.com/aliyashz/DSP/7f847be46f7b0fee9952b39b80d6e102ed1fcb5b/250k_rndm_zinc_drugs.csv"
+)
+
+# Custom CSS to style the title
+st.markdown(""" <style> .font {font-size:50px ; font-family: 'Playfair Display'; color: #FF4242;} </style> """, unsafe_allow_html=True)
+
+# Title
+st.markdown('<p class="font"> 🧬DRUG DISCOVERY🧬 </p>', unsafe_allow_html=True)
+
+menu = ["Home 🏚️", "About❓", "Analysis 📊", "Model 🤖"]
+choice = st.sidebar.selectbox("Navigation", menu)
+
 
 # Display the selected tab content
 if selected_tab == "Home":
